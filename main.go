3c77f593e4473100ad36f783528c36e6cf2ed93e
孙通 <17701253801@163.com>
2024-05-03 08:08:47 +0800

    UPDATE：code style
    启动端口增加错误判断

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	imageCompression "github.com/kiwi633/go-demo/image"
-	_ "github.com/lib/pq"
 )
 
 func main() {
 	router := gin.Default()
-	// 为 multipart forms 设置较低的内存限制 (默认是 32 MiB)
-	router.MaxMultipartMemory = 32 << 20 // 8 MiB
+	router.MaxMultipartMemory = 64 << 20 // 8 MiB
 	router.POST("/upload", func(c *gin.Context) {
 		yasuoType := c.Query("yasuoType")
 		if yasuoType == "resize" {
@@ -19,6 +17,8 @@ func main() {
 			imageCompression.FileUploadThumbnail(c)
 		}
 	})
-	router.Run(":8081")
-
+	err := router.Run(":8081")
+	if err != nil {
+		return
+	}
 }
